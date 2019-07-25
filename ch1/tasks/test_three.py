c6b40a4e5fb75a969c0c6c055ebb379b6cdef52c
nchikun <donkonchi@gmail.com>
2019-07-26 01:26:16 +0900

    test_fort.pyの追加

--- ch1/tasks/test_three.py
+++ ch1/tasks/test_three.py
@@ -8,3 +8,12 @@ def test_defaults():
     t1 = Task()
     t2 = Task(None, None, False, None)
     assert t1 == t2
+
+
+def test_member_access():
+    t = Task('buy milk', 'brain')
+    assert t.summary == 'buy milk'
+    assert t.owner == 'brain'
+    assert (t.done, t.id) == (False, None)
+
+
